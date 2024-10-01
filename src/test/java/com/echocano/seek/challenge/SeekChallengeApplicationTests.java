2aab33738bdeea92df74cbad823b9cd1021e1645
Elthon Chocano <echocano@novopayment.com>
2024-10-01 08:09:37 -0700

    fix: application to yml

---
+++ src/test/java/com/echocano/seek/challenge/SeekChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.echocano.seek.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeekChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
